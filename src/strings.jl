4b810e03739f126cd24e813118a5e7c33b178ab3
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-02-10 11:30:14 +0100

    Update doc. and creation of arrays with undefined contents

--- src/strings.jl
+++ src/strings.jl
@@ -7,37 +7,43 @@
 #
 # This file is part of "AndorCameras.jl" released under the MIT license.
 #
-# Copyright (C) 2017, Éric Thiébaut.
+# Copyright (C) 2017-2019, Éric Thiébaut.
 #
 
 """
-    widestring(str, len = strlen(str))
+```julia
+widestring(str, len = strlen(str))
+```
 
-yields a vector of wide characters (`Cwchar_t`) with the contents of the
-string `str` and properly zero-terminated.  This buffer is independent from
-the input string and its contents can be overwritten.  An error is thrown
-if `str` contains any embedded NUL characters (which would cause the string
-to be silently truncated if the C routine treats NUL as the terminator).
+yields a vector of wide characters (`Cwchar_t`) with the contents of the string
+`str` and properly zero-terminated.  This buffer is independent from the input
+string and its contents can be overwritten.  An error is thrown if `str`
+contains any embedded NULL characters (which would cause the string to be
+silently truncated if the C routine treats NULL as the terminator).
 
-An alternative (without the checking of embedded NUL characters) is:
+An alternative (without the checking of embedded NULL characters) is:
 
-    push!(transcode(Cwchar_t, str), convert(Cwchar_t, 0))
+```julia
+push!(transcode(Cwchar_t, str), convert(Cwchar_t, 0))
+```
 
 This method is used to implement the `@L_str` macro which converts a
 literal string into a wide character string.  For instance:
 
-    L"EventSelector"
+```julia
+L"EventSelector"
+```
 
 """
 function widestring(str::AbstractString,
-                 len::Integer = length(str)) :: Array{WideChar}
-    buf = Array{WideChar}(len + 1)
+                    len::Integer = length(str)) :: Array{WideChar}
+    buf = Array{WideChar}(undef, len + 1)
     i = 0
     @inbounds for c in str
         if i ≥ len
             break
         end
-        c != '\0' || error("string must not have embedded NUL characters")
+        c != '\0' || error("string must not have embedded NULL characters")
         i += 1
         buf[i] = c
     end
@@ -51,7 +57,7 @@ end
 widestring(sym::Symbol) = widestring(string(sym))
 
 function widestringtostring(C::Array{WideChar}) :: String
-    buf = Array{Char}(0)
+    buf = Char[]
     @inbounds for c in C
         if c == zero(WideChar)
             break
